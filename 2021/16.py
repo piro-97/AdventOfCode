2895d6052272f155b5eeb8c2f92ed7051d6c7c0e
piro-97 <mpirola97@gmail.com>
2021-12-16 12:56:30 +0100

    some improvements

--- 2021/16.py
+++ 2021/16.py
@@ -1,52 +1,21 @@
-from numpy import multiply
 import utils
+from functools import reduce
 
 DAY = 16
 
+OPERATIONS = [
+    lambda x,y : x+y,           # sum subpackets
+    lambda x,y : x*y,           # multiply subpackets
+    lambda x,y : min(x,y),      # minimum of subpackets
+    lambda x,y : max(x,y),      # maximum of subpackets
+    None,                       # value packet
+    lambda x,y : int(x>y),      # greater then
+    lambda x,y : int(x<y),      # less then
+    lambda x,y : int(x==y),     # equal to
+]
 
-def sum_packets(packet :dict):
-    tot = 0
-    for p in packet["subpackets"]:
-        tot += p["value"]
-    packet["value"] = tot
-
-def multiply_packets(packet :dict):
-    tot = 1
-    for p in packet["subpackets"]:
-        tot *= p["value"]
-    packet["value"] = tot
-    
-def min_packets(packet :dict):
-    mini = float('inf')
-    for p in packet["subpackets"]:
-        if p["value"] < mini:
-            mini = p["value"]
-    packet["value"] = mini
-
-def max_packets(packet :dict):
-    maxi = float(0)
-    for p in packet["subpackets"]:
-        if p["value"] > maxi:
-            maxi = p["value"]
-    packet["value"] = maxi
-
-def greater_packets(packet :dict):
-    sub_p = packet["subpackets"]
-    packet["value"] = int( sub_p[0]["value"] > sub_p[1]["value"] )
-    
-def less_packets(packet :dict):
-    sub_p = packet["subpackets"]
-    packet["value"] = int( sub_p[0]["value"] < sub_p[1]["value"] )
-
-def equal_packets(packet :dict):
-    sub_p = packet["subpackets"]
-    packet["value"] = int( sub_p[0]["value"] == sub_p[1]["value"] )
-
-
-OPERATIONS = [ sum_packets, multiply_packets, min_packets, max_packets, None, greater_packets, less_packets, equal_packets ]
-
-
-def decode_value(string :str) -> tuple:     # returns decimal value and length of string consumed
+def decode_value(string :str) -> tuple:
+    """returns decimal value and length of string consumed"""
     s = ""
     consumed = 0
     while string[consumed] == "1":  # part A and B of value (if present)
@@ -57,49 +26,45 @@ def decode_value(string :str) -> tuple:     # returns decimal value and length o
 
 
 def decode(string :str) -> dict:
+    """returns a packet as a dictionary"""
     p_version = int(string[:3], 2)
     p_type = int(string[3:6], 2)
+    p_value, p_length, p_operation = 0, 6, None
+    p_subpackets = []
     
-    if p_type == 4:
-        p_value, p_length = decode_value(string[6 :])
-        return {
-            "version" : p_version,
-            "type" : p_type,
-            "value" : p_value,
-            "length" : p_length + 6,
-            "subpackets" : [],
-        }
-
-    else:
+    if p_type == 4: # value packet
+        p_value, length = decode_value(string[6 :])
+        p_length += length
+    
+    else:   # operation packet
         p_length_type = string[6]
-        subpackets = []
-        
-        if p_length_type == "0":
-            sub_length = int(string[7:22], 2)
-            curr_length = 0
-            while curr_length < sub_length:
-                p = decode(string[22 + curr_length :])
-                subpackets.append(p)
-                curr_length += p["length"]
-            p_length = curr_length + 22
-        
-        else:
-            sub_num = int(string[7:18], 2)
-            curr_length = 0
-            while sub_num > 0:
-                p = decode(string[18 + curr_length :])
-                subpackets.append(p)
-                curr_length += p["length"]
-                sub_num -= 1
-            p_length = curr_length + 18
-    
-        return {
-            "version": p_version,
-            "type" : p_type,
-            "subpackets" : subpackets,
-            "length" : p_length,
-            "operation" : OPERATIONS[p_type]
-        }
+        p_operation = OPERATIONS[p_type]
+
+        if p_length_type == "0":    # subpackets defined by length of string
+            sub_p_length = int(string[7:22], 2)
+            p_length += 16
+            while p_length - 22 < sub_p_length:
+                p = decode(string[p_length :])
+                p_subpackets.append(p)
+                p_length += p["length"]
+            
+        else:   # subpackets defined by number of subpackets
+            sub_p_num = int(string[7:18], 2)
+            p_length += 12
+            while sub_p_num > 0:
+                p = decode(string[p_length :])
+                p_subpackets.append(p)
+                p_length += p["length"]
+                sub_p_num -= 1
+
+    return {
+        "version": p_version,
+        "type" : p_type,
+        "subpackets" : p_subpackets,
+        "operation" : p_operation,
+        "value" : p_value,
+        "length" : p_length,
+    }
 
 
 def sum_versions(packet :dict) -> int:
@@ -112,19 +77,21 @@ def sum_versions(packet :dict) -> int:
 def compute(packet :dict) -> int:
     if not packet["subpackets"]:
         return packet["value"]
-    
+
     for p in packet["subpackets"]:
         p["value"] = compute(p)
+    
+    packet["value"] = reduce( packet["operation"], map(lambda p : p["value"], packet["subpackets"]) )
 
-    packet["operation"](packet)
     return packet["value"]
 
 
 message_hex = utils.read_input(DAY)[0]
 message_dec = int(message_hex, 16)
 message_bin = format(message_dec, f'0>{len(message_hex)*4}b')
+packet_hierarchy = decode(message_bin)
 
 # part 1
-packet_hierarchy = decode(message_bin)
 utils.print_answer(1, sum_versions(packet_hierarchy))
+# part 2
 utils.print_answer(2, compute(packet_hierarchy))
